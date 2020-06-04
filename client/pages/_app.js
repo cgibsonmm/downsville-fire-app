d5244c1ce08ca76da185e4358e36328e38ab2f37
corey gibson <cgibsonmm@gmail.com>
2020-06-04 09:31:07 -0400

    added redux

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,12 @@
 import "../assets/css/styles.css";
 
+import { Provider } from "react-redux";
+import store from "../redux/store";
+
 export default function App({ Component, pageProps }) {
   return (
-    <div>
+    <Provider store={store}>
       <Component {...pageProps} />
-    </div>
+    </Provider>
   );
 }
