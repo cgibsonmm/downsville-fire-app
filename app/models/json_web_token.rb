18d3eb4a82d8638704fdaa152300c6a01b8d2af0
corey gibson <cgibsonmm@gmail.com>
2020-06-18 23:32:38 -0400

    testing other casing for key

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -3,10 +3,10 @@
 class JsonWebToken
   def self.encode(payload)
     expiration = 2.weeks.from_now.to_i
-    JWT.encode payload.merge(exp: expiration), Rails.application.credentials.fetch(:SECRET_KEY_BASE)
+    JWT.encode payload.merge(exp: expiration), Rails.application.credentials.fetch(:secret_key_base)
   end
 
   def self.decode(token)
-    JWT.decode(token, Rails.application.credentials.fetch(:SECRET_KEY_BASE)).first
+    JWT.decode(token, Rails.application.credentials.fetch(:secret_key_base)).first
     end
 end
